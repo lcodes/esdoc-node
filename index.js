eda74ce4396b6e832bdcab659cdf204d32b396e3
Gontran Baerts <gontran.baerts@ihealthlabs.com>
2017-01-11 12:49:03 +0100

    Use global replace for "exports.some = some;"

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.onHandleCode = function onHandleCode(ev) {
              'export default ')
     .replace(/exports\s*\.\s*([_\d\w]+)\s*=\s*(class|function\*?)\s+\1/g,
              'export $2 $1')
-    .replace(/exports\s*\.\s*([_\d\w]+)\s*=\s*\1\s*;/,
+    .replace(/exports\s*\.\s*([_\d\w]+)\s*=\s*\1\s*;/g,
              'export { $1 };')
     .replace(/exports\s*\.\s*([_\d\w]+)\s*=/,
              'export let $1 =');
